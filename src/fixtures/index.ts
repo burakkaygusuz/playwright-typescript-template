e60c4b03222a33df376e4153b62a97f70927f540
Burak Kaygusuz <kaygusuzburak@gmail.com>
2024-03-07 10:58:53 +0300

    Remove default property

--- src/fixtures/index.ts
+++ src/fixtures/index.ts
@@ -2,7 +2,7 @@ import { test as base } from '@playwright/test';
 
 export const test = base.extend({
   page: async ({ baseURL, page }, use) => {
-    await page.goto(baseURL, { waitUntil: 'load' });
+    await page.goto(baseURL);
     await use(page);
   },
 });
